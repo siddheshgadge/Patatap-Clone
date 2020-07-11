4651a66028b97f10a26c4fbffa58bb33f20dada8
Siddhesh Sharad Gadge <37931270+siddheshgadge@users.noreply.github.com>
2020-07-11 22:51:51 +0530

    Delete patatap.js

--- patatap.js
+++
@@ -1,8 +0,0 @@
-var intro = document.querySelector("div");
-var canvas = document.querySelector("canvas");
-intro.addEventListener("click",function(){
-	intro.style.display="none";	
-	intro.remove();
-	canvas.classList.add("open");
-});
-
